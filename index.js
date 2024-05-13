6093703ae00fb9e3408380ad8bd57886c71b40dc
nithilesh <120772391+Nithilesh2@users.noreply.github.com>
2024-05-13 15:54:32 +0530

    added geolocation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let loc = document.querySelector("#location");
 let location_value = document.querySelector(".location");
 let button = document.querySelector("#button");
+let buttonCurrentLocation = document.querySelector("#buttonCurrentLocation");
 let sun = document.querySelector("#sun");
 let storm = document.querySelector("#storm");
 let rainy_day = document.querySelector("#rainy_day");
@@ -15,9 +16,6 @@ let winddirectionvalue = document.querySelector(".winddirectionvalue");
 let winddegreevalue = document.querySelector(".winddegreevalue");
 let displayer = document.querySelector(".having");
 
-// https://api.openweathermap.org/data/2.5/weather?q=kerala&appid=4ee63614fb7c0c6d56805c75acc5c90e
-
-
 let key = "4ee63614fb7c0c6d56805c75acc5c90e";
 
 button.addEventListener("click", () => {
@@ -25,132 +23,180 @@ button.addEventListener("click", () => {
   const fetchAPI = async (url) => {
     const response = await fetch(url + `&appid=${key}`);
     const data = await response.json();
-    console.log(data);
 
-    
-
-    if(data.message === "city not found"){
+    if (data.message === "city not found") {
       location_value.innerHTML = data.message;
-    }
-
-    else{
+    } else {
       location_value.innerHTML = data.name;
-    tempcvalue.innerHTML = data.main.temp_min;
-    tempfvalue.innerHTML = data.main.temp_max;
-    windkmphvalue.innerHTML = data.wind.speed + " kmph";
-    windmphvalue.innerHTML = data.wind.gust + " kt";
-    winddirectionvalue.innerHTML = data.main.humidity + " %";
-    winddegreevalue.innerHTML = data.wind.deg + "°";
-    displayer.innerHTML = "having " + data.weather[0].main + " weather";
+      tempcvalue.innerHTML = data.main.temp_min;
+      tempfvalue.innerHTML = data.main.temp_max;
+      windkmphvalue.innerHTML = data.wind.speed + " kmph";
+      windmphvalue.innerHTML = data.wind.gust + " kt";
+      winddirectionvalue.innerHTML = data.main.humidity + " %";
+      winddegreevalue.innerHTML = data.wind.deg + "°";
+      displayer.innerHTML = "having " + data.weather[0].main + " weather";
 
-    if(data.weather[0].main === "Haze"){
-      fog.style.display = "block";
-      cloudy.style.display = "none";
-      sun.style.display = "none";
-      storm.style.display = "none";
-      rainy_day.style.display = "none";
-      clouds.style.display = "none";
+      if (data.weather[0].main === "Haze") {
+        fog.style.display = "block";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+        storm.style.display = "none";
+        rainy_day.style.display = "none";
+        clouds.style.display = "none";
+      } else if (data.weather[0].main === "Clear") {
+        cloudy.style.display = "block";
+        sun.style.display = "none";
+        storm.style.display = "none";
+        rainy_day.style.display = "none";
+        clouds.style.display = "none";
+        fog.style.display = "none";
+      } else if (data.weather[0].main === "Clouds") {
+        clouds.style.display = "block";
+        fog.style.display = "none";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+        storm.style.display = "none";
+        rainy_day.style.display = "none";
+      } else if (data.weather[0].main === "Rain") {
+        rainy_day.style.display = "block";
+        clouds.style.display = "none";
+        fog.style.display = "none";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+        storm.style.display = "none";
+      } else {
+        storm.style.display = "block";
+        rainy_day.style.display = "none";
+        clouds.style.display = "none";
+        fog.style.display = "none";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+      }
     }
-    else if(data.weather[0].main === "Clear"){
-      cloudy.style.display = "block";
-      sun.style.display = "none";
-      storm.style.display = "none";
-      rainy_day.style.display = "none";
-      clouds.style.display = "none";
-      fog.style.display = "none";
-    }
-    else if(data.weather[0].main === "Clouds"){
-      clouds.style.display = "block";
-      fog.style.display = "none";
-      cloudy.style.display = "none";
-      sun.style.display = "none";
-      storm.style.display = "none";
-      rainy_day.style.display = "none";
-    }
-    else if(data.weather[0].main === "Rain"){
-      rainy_day.style.display = "block";
-      clouds.style.display = "none";
-      fog.style.display = "none";
-      cloudy.style.display = "none";
-      sun.style.display = "none";
-      storm.style.display = "none";
-    }
-    else{
-      storm.style.display = "block";
-      rainy_day.style.display = "none";
-      clouds.style.display = "none";
-      fog.style.display = "none";
-      cloudy.style.display = "none";
-      sun.style.display = "none";
-    }
-    }
-
   };
   fetchAPI(url);
+});
+
+async function btnCrntLocation(latitude, longitude) {
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}`;
+  const fetchAPI = async (url) => {
+    const response = await fetch(url + `&appid=${key}`);
+    const data = await response.json();
+
+    if (data.message === "city not found") {
+      location_value.innerHTML = data.message;
+    } else {
+      location_value.innerHTML = data.name;
+      tempcvalue.innerHTML = data.main.temp_min;
+      tempfvalue.innerHTML = data.main.temp_max;
+      windkmphvalue.innerHTML = data.wind.speed + " kmph";
+      windmphvalue.innerHTML = data.wind.gust + " kt";
+      winddirectionvalue.innerHTML = data.main.humidity + " %";
+      winddegreevalue.innerHTML = data.wind.deg + "°";
+      displayer.innerHTML = "having " + data.weather[0].main + " weather";
+
+      if (data.weather[0].main === "Haze") {
+        fog.style.display = "block";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+        storm.style.display = "none";
+        rainy_day.style.display = "none";
+        clouds.style.display = "none";
+      } else if (data.weather[0].main === "Clear") {
+        cloudy.style.display = "block";
+        sun.style.display = "none";
+        storm.style.display = "none";
+        rainy_day.style.display = "none";
+        clouds.style.display = "none";
+        fog.style.display = "none";
+      } else if (data.weather[0].main === "Clouds") {
+        clouds.style.display = "block";
+        fog.style.display = "none";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+        storm.style.display = "none";
+        rainy_day.style.display = "none";
+      } else if (data.weather[0].main === "Rain") {
+        rainy_day.style.display = "block";
+        clouds.style.display = "none";
+        fog.style.display = "none";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+        storm.style.display = "none";
+      } else {
+        storm.style.display = "block";
+        rainy_day.style.display = "none";
+        clouds.style.display = "none";
+        fog.style.display = "none";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+      }
+    }
+  };
+  fetchAPI(url);
+}
+
+buttonCurrentLocation.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(
+    (res) => {
+      btnCrntLocation(res.coords.latitude, res.coords.longitude);
+    }
+  );
 });
 
 loc.addEventListener("keypress", (event) => {
   if (event.key == "Enter") {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${loc.value}`;
     const fetchAPI = async (url) => {
-    const response = await fetch(url + `&appid=${key}`);
-    const data = await response.json();
-    console.log(data);
+      const response = await fetch(url + `&appid=${key}`);
+      const data = await response.json();
 
-    location_value.innerHTML = data.name;
-    tempcvalue.innerHTML = data.main.temp_min;
-    tempfvalue.innerHTML = data.main.temp_max;
-    windkmphvalue.innerHTML = data.wind.speed + " kmph";
-    windmphvalue.innerHTML = data.wind.gust + " kt";
-    winddirectionvalue.innerHTML = data.main.humidity + " %";
-    winddegreevalue.innerHTML = data.wind.deg + "°";
-    displayer.innerHTML = "having " + data.weather[0].main + " weather";
+      location_value.innerHTML = data.name;
+      tempcvalue.innerHTML = data.main.temp_min;
+      tempfvalue.innerHTML = data.main.temp_max;
+      windkmphvalue.innerHTML = data.wind.speed + " kmph";
+      windmphvalue.innerHTML = data.wind.gust + " kt";
+      winddirectionvalue.innerHTML = data.main.humidity + " %";
+      winddegreevalue.innerHTML = data.wind.deg + "°";
+      displayer.innerHTML = "having " + data.weather[0].main + " weather";
 
-    
-    if(data.weather[0].main === "Haze"){
-      fog.style.display = "block";
-      cloudy.style.display = "none";
-      sun.style.display = "none";
-      storm.style.display = "none";
-      rainy_day.style.display = "none";
-      clouds.style.display = "none";
-    }
-    else if(data.weather[0].main === "Clear"){
-      cloudy.style.display = "block";
-      sun.style.display = "none";
-      storm.style.display = "none";
-      rainy_day.style.display = "none";
-      clouds.style.display = "none";
-      fog.style.display = "none";
-    }
-    else if(data.weather[0].main === "Clouds"){
-      clouds.style.display = "block";
-      fog.style.display = "none";
-      cloudy.style.display = "none";
-      sun.style.display = "none";
-      storm.style.display = "none";
-      rainy_day.style.display = "none";
-    }
-    else if(data.weather[0].main === "Rain"){
-      rainy_day.style.display = "block";
-      clouds.style.display = "none";
-      fog.style.display = "none";
-      cloudy.style.display = "none";
-      sun.style.display = "none";
-      storm.style.display = "none";
-    }
-    else{
-      storm.style.display = "block";
-      rainy_day.style.display = "none";
-      clouds.style.display = "none";
-      fog.style.display = "none";
-      cloudy.style.display = "none";
-      sun.style.display = "none";
-    }
-
+      if (data.weather[0].main === "Haze") {
+        fog.style.display = "block";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+        storm.style.display = "none";
+        rainy_day.style.display = "none";
+        clouds.style.display = "none";
+      } else if (data.weather[0].main === "Clear") {
+        cloudy.style.display = "block";
+        sun.style.display = "none";
+        storm.style.display = "none";
+        rainy_day.style.display = "none";
+        clouds.style.display = "none";
+        fog.style.display = "none";
+      } else if (data.weather[0].main === "Clouds") {
+        clouds.style.display = "block";
+        fog.style.display = "none";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+        storm.style.display = "none";
+        rainy_day.style.display = "none";
+      } else if (data.weather[0].main === "Rain") {
+        rainy_day.style.display = "block";
+        clouds.style.display = "none";
+        fog.style.display = "none";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+        storm.style.display = "none";
+      } else {
+        storm.style.display = "block";
+        rainy_day.style.display = "none";
+        clouds.style.display = "none";
+        fog.style.display = "none";
+        cloudy.style.display = "none";
+        sun.style.display = "none";
+      }
     };
     fetchAPI(url);
-
-}
+  }
 });
-
